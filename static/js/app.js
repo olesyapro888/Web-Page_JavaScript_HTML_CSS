f354b93586c88a5bb549132bdc83a7361a86acd7
Olesya Irkhina <itekkie888@gmail.com>
2021-09-12 14:25:08 -0400

    update app

--- static/js/app.js
+++ static/js/app.js
@@ -24,8 +24,7 @@ function buildTable(data) {
 }
 
 // 1. Create a variable to keep track of all the filters as an object.
-
-
+let filters = {}
 // 3. Use this function to update the filters. 
 function updateFilters() {
 
